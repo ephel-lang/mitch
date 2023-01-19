3f75cc275343c8af18541fe3a6c96c2388699c37
d-plaindoux <d.plaindoux@free.fr>
2023-01-19 13:44:19 +0100

    First commit

---
+++ lib/mitch/mitch.mli
@@ -0,0 +1,3 @@
+module Lang = Mitch_lang
+module Vm = Mitch_vm
+module System = Mitch_system
---
+++ lib/mitch/system/expander.ml
@@ -0,0 +1,21 @@
+open Mitch_vm
+
+let rec expand_sequence =
+  let open Objcode in
+  function
+  | IF_LEFT (l, r) :: s when List.length s > 0 -> [ IF_LEFT (l @ s, r @ s) ]
+  | a :: l -> expand_instruction a :: expand_sequence l
+  | [] -> []
+
+and expand_instruction =
+  let open Objcode in
+  function
+  | LAMBDA (n, l) -> LAMBDA (n, expand_sequence l)
+  | IF_LEFT (l, r) -> IF_LEFT (expand_sequence l, expand_sequence r)
+  | o -> o
+
+let rec expand o =
+  let o' = expand_sequence o in
+  if o' = o then o' else expand o'
+
+let run o = expand o
